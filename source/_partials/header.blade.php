fae1ea53bc6f3d69a73a6803d207a02a9e430426
Benjamin H Boruff <benboruff@gmail.com>
2021-06-24 00:36:53 -0400

    basic header with nav

---
+++ source/_partials/header.blade.php
@@ -0,0 +1,11 @@
+<header class="flex flex-row justify-between space-x-4 bg-white py-6 px-6">
+    <a class="block" href="/">
+        <span class="sr-only">Benjamin H Boruff</span>
+        {{ $page->title }}
+    </a>
+    <nav class="flex flex-row space-x-6 font-semibold">
+        <a href="#" class="text-indigo-600 hover:underline">Home</a>
+        <a href="#" class="text-indigo-600 hover:underline">About</a>
+        <a href="#" class="text-indigo-600 hover:underline">Contact</a>
+    </nav>
+</header>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+<div class="container mx-auto">
+    <h1 class="text-3xl font-bold text-center">Hello world!</h1>
 </div>
 @endsection
