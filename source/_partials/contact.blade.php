dc3707d106bbfe7ca5612e144a6b38f3b5438dc4
Benjamin Boruff <benboruff@gmail.com>
2021-06-29 12:06:09 -0400

    Polish data (#12)
    * added top margin to sidenav and some text spacing on titles
    
    * added some top margin to main content

--- source/_partials/contact.blade.php
+++ source/_partials/contact.blade.php
@@ -1,5 +1,5 @@
 <div id="contact" class="p-2 lg:pl-2 lg:pt-4 lg:pr-4">
-    <p class="text-center text-lg">Contact</p>
+    <p class="text-center text-lg tracking-widest">Contact</p>
     <hr/>
     <p class="prose p-2 mx-auto"> Some verbiage! </p>
 </div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -6,20 +6,20 @@ pagination:
 @extends('_layouts.main')
 
 @section('body')
-<div class="flex-auto container mx-auto">
+<div class="mt-6">
     {{-- <a href="/#"><h1 class="text-3xl text-center pt-4 pb-2 mg:hidden">{{ $page->site_title }}</h1></a> --}}
     <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4">
         <div>
             <div class="p-2 lg:p-4">
-                <p class="text-center text-lg">Projects</p>
+                <p class="text-center text-lg tracking-widest">Projects</p>
                 <hr/>
             </div>
             <ul class="flex flex-col p-4">
                 @foreach ($pagination->items as $project)
                 <div>
                     <li class="rounded overflow-hidden shadow-lg py-4">
-                        <a class="text-green-600 hover:underline px-2" href="{{ $project->getUrl() }}">{{ $project->title }}</a>
-                        <p class="px-2">{{ $project->description }}</p>
+                        <a class="px-2 text-green-600 tracking-wide hover:underline " href="{{ $project->getUrl() }}">{{ $project->title }}</a>
+                        <p class="px-2 prose">{{ $project->description }}</p>
                     </li>
                 </div>
                 @endforeach
