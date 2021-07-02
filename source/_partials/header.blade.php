712f7f1158171a58a78a78df409669127bc88e5b
Benjamin H Boruff <benboruff@gmail.com>
2021-07-02 17:30:15 -0400

    changed header/nav text and bg colors

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-gray-600 shadow-lg">
         <nav class="flex flex-row">
             <div id="menu-arrow" class="mx-4 my-5 lg:hidden">
-                <svg class="lg:invisible fill-current text-yellow-200 " viewBox='0 0 140 140' width='24' height='24' xmlns='http://www.w3.org/2000/svg'>
+                <svg class="lg:invisible fill-current text-orange-300 " viewBox='0 0 140 140' width='24' height='24' xmlns='http://www.w3.org/2000/svg'>
                     <g>
                         <path d='m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 
                         1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z' fill='' />
@@ -10,11 +10,11 @@
                 </svg>
             </div>
             <div class="mt-3 mb-2 pr-14 flex-grow lg:p-4">
-                <a class="block text-center tracking-wide text-yellow-200 font-medium" href="/">
+                <a class="block text-center tracking-wide text-orange-300 font-medium" href="/">
                     <span class="sr-only">Benjamin H Boruff</span>
                     {{ $page->site_title }}
                 </a>
-                <div class="text-center text-xs tracking-wide text-yellow-200">Developer</div>
+                <div class="text-center text-xs tracking-wide text-orange-300">Developer</div>
             </div>
         </nav>
     </div>
--- source/_partials/sidebar.blade.php
+++ source/_partials/sidebar.blade.php
@@ -1,21 +1,12 @@
 <div id="sidebar" class="fixed w-36 bg-gray-600  top-0 z-40 transform -translate-x-full lg:translate-x-0">
     <nav>
         <div class="grid grid-cols-1 min-h-screen">
-            <div class="">
-                <div class="hidden">
-                    <a class="block pt-4 text-center text-yellow-200" href="/">
-                        <span class="sr-only">Benjamin H Boruff</span>
-                        {{ $page->site_title }}
-                    </a>
-                    <div class="text-center text-xs text-yellow-200">Developer</div>
-                </div>
-                <div class="mt-20">
-                    <ul id="menu" class="grid grid-flow-row text-center font-semibold">
-                        <li class="pt-8 pb-4"><a id="side-home" href="/#home" class="text-yellow-200 tracking-wider hover:underline">Home</a></li>
-                        <li class="py-4"><a id="side-about" href="/#about" class="text-yellow-200 tracking-wider hover:underline">About</a></li>
-                        <li class="py-4"><a id="side-contact" href="/#contact" class="text-yellow-200 tracking-wider hover:underline">Contact</a></li>
-                    </ul>
-                </div>
+            <div class="mt-20">
+                <ul id="menu" class="grid grid-flow-row text-center font-semibold">
+                    <li class="pt-8 pb-4"><a id="side-home" href="/#home" class="text-lime-200 tracking-wider hover:underline">Home</a></li>
+                    <li class="py-4"><a id="side-about" href="/#about" class="text-lime-200 tracking-wider hover:underline">About</a></li>
+                    <li class="py-4"><a id="side-contact" href="/#contact" class="text-lime-200 tracking-wider hover:underline">Contact</a></li>
+                </ul>
             </div>
         </div>
     </nav>
